f04d8b826a25f038cf4d8a71525e7ed022bdb2d8
msprogrammer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-02-12 23:20:35 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# This code write by ms.nope
+echo "                                   "
+echo "█░░░█ █▀▀ █▀▀▄ ▀▀█▀▀ █▀▀ █▀▀ ▀▀█▀▀ "
+echo "█▄█▄█ █▀▀ █▀▀▄ ░░█░░ █▀▀ ▀▀█ ░░█░░ "
+echo "░▀░▀░ ▀▀▀ ▀▀▀░ ░░▀░░ ▀▀▀ ▀▀▀ ░░▀░░ "
+echo "                                   "
+echo "          (webtest installing...)  "
+apt-get install python3 
+apt-get install python
+echo "finish !"
