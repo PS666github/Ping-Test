cb289ab69e75c3f8fb0aafe3183417dc387e6f30
ms.programmer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-02-14 01:03:40 -0700

    Add files via upload

---
+++ update.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# This code write by (ms.nope)
+import os
+import time
+os.system("clear")
+print("""
+
+▀█▀ █▀▀ █▀ ▀█▀ █░█░█ █▀▀ █▄▄
+░█░ ██▄ ▄█ ░█░ ▀▄▀▄▀ ██▄ █▄█""")
+time.sleep(1)
+print("portscan update...")
+os.system("cd ..")
+os.system("sudo rm -r testweb/")
+time.sleep(1)
+os.system("cd ..") 
+os.system("git clone https://github.com/msprogrammer2938/testweb.git")
+time.sleep(1)
+os.system("clear")
+print("finish!")
+time.sleep(1)
+b = str(input("press Enter to run (testweb)... "))
+if(str(b) == ''):
+  os.system("cd testweb/")
+time.sleep(1)
+os.system("clear")
+print("run testweb...")
+time.sleep(1)
+print("""
+The installing finish i open directory (testweb) !
+""")
+os.system("ls")
+exit(1)
+# testweb
