98411a77a1d9179f800d368677e44c39cb2c2bc9
Ms.programmer <78996423+msprogrammer2938@users.noreply.github.com>
2021-04-19 09:09:35 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 # This code write by ms.nope
+clear
 echo "                                   "
 echo "█░░░█ █▀▀ █▀▀▄ ▀▀█▀▀ █▀▀ █▀▀ ▀▀█▀▀ "
 echo "█▄█▄█ █▀▀ █▀▀▄ ░░█░░ █▀▀ ▀▀█ ░░█░░ "
 echo "░▀░▀░ ▀▀▀ ▀▀▀░ ░░▀░░ ▀▀▀ ▀▀▀ ░░▀░░ "
 echo "                                   "
 echo "          (webtest installing...)  "
+echo ""
 chmod +x webtest.py
 sudo apt-get install python3 
 sudo apt-get install python
+echo ""
 echo "finish !"
+echo ""
+echo "usage:
+            ./webtest.py"
